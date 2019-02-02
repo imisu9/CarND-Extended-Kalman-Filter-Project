e996a5b390af778f988c05ccca6f4630b8ad9c95
Seo, Jungwoo <36559904+imisu9@users.noreply.github.com>
2019-02-02 21:38:43 +0900

    feat: complete update

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -155,10 +155,16 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // TODO: Radar updates
-
+    Tools tools;
+    Hj_ = tools.CalculateJacobian(ekf_.x_);
+    ekf_.H_ = Hj_;
+    ekf_.R_ = R_radar_;
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
     // TODO: Laser updates
-
+    ekf_.H_ = H_laser;
+    ekf_.R_ = R_laser;
+    ekf_.Update(measurement_pack.raw_measurements_);
   }
 
   // print the output
